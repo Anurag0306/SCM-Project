35b24331b16ca1b1dd47ba6596131ac1294b437c
Anurag0306 <karamveer0306@gmail.com>
2025-01-07 13:13:51 +0530

    correct workbench

--- scm2.0/src/main/java/com/scm/entities/Users.java
+++ scm2.0/src/main/java/com/scm/entities/Users.java
@@ -1,47 +1,47 @@
 package com.scm.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Setter; 
-import lombok.Getter; 
-import lombok.Builder;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Entity(name = "user")
+@Entity
 @Table(name = "users")
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class Users {
 
     @Id
     private String userId;
 
-    @Column(name = "user_name", nullable =false)
+    @Column(name = "user_name", nullable = false)
     private String name;
 
     @Column(name = "email", nullable = false, unique = true)
-    private String email; 
+    private String email;
 
     private String password;
 
-    @Column(length =10000)
+    @Column(length = 1000)
     private String about;
 
-    @Column(length =10000)
+    @Column(name = "profile_pic", length = 500)
     private String profilePic;
 
+    @Column(name = "phone_number", length = 15)
     private String phoneNumber;
-    //Informations
-    private boolean enabled = false;
-    private boolean emailVerified = false;
-    private boolean phoneNumberVerified = false;
 
-    //SELF, GOOGLE, FACEBOOK, GITHUB
-    private  providers provider = providers.SELF;
+    @Column(nullable = false)
+    private boolean enabled;
 
+    @Column(name = "email_verified", nullable = false)
+    private boolean emailVerified;
+
+    @Column(name = "phone_number_verified", nullable = false)
+    private boolean phoneNumberVerified;
+
+    // @Enumerated(EnumType.STRING)
+    // @Column(nullable = false)
+  
 }
